71832ec13ece1e41ef32f52565b8586c006a85a5
Sean <sean@hydreon.com>
2015-11-08 15:04:35 -0500

    Update Main.php

--- src/Sean_M/Disguise/Main.php
+++ src/Sean_M/Disguise/Main.php
@@ -1,1 +1,43 @@
+<?php
+namespace Sean_M\Disguise;
 
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener {
+
+  public $config;
+
+    public function onLoad() {
+        $this->getLogger()->info(TextFormat::GREEN . "Loading Disguise by Sean_M...");
+    }
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->config = $this->getConfig()->getAll();
+        $this->getLogger()->info(TextFormat::GREEN . "Disguise by Sean_M enabled!");
+    }
+
+    public function onDisable() {
+        $this->getLogger()->info(TextFormat::RED . "Disguise by Sean_M disabled!");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label,array $args){
+        if(strtolower($cmd->getName()) === "disguise") {
+            if($sender instanceof Player) {
+                if($sender->hasPermission("disguise.command")) {
+                    $input = array("Apple", "Orange", "Banana", "Pear");
+                    $rand_keys = array_rand($input, 1);
+                        echo $input[$rand_keys[1]] . "\n";
+// Change line above to get the random word & set player name later.
+                return true;
+                }else{
+                    $sender->sendMessage(TextFormat::RED . "You don't have permissions to use this command.");
+            }
+        }
+    }
+}
